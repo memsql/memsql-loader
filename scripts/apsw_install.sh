b82ff1249d67591c5bd27c1a6ffcc4b42b5a61a7
Hurshal Patel <hurshal@memsql.com>
2017-02-22 11:26:39 -0800

    bump apsw version

--- scripts/apsw_install.sh
+++ scripts/apsw_install.sh
@@ -2,12 +2,12 @@
 
 ##### UPDATE memsql_loader/main.py if you change this file!
 
-pip freeze | grep -q "apsw.git@1be8f228f8b337f33f1a88ee55f61915133dd9b4"
+pip freeze | grep -q "apsw.git@26b61c39a98db3ccad7f852adc944b0b8e94c242"
 ret=$?
 if [ "$ret" != "0" ]
 then
     pip install \
-        -e "git+git://github.com/rogerbinns/apsw.git@1be8f228f8b337f33f1a88ee55f61915133dd9b4#egg=apsw" \
+        -e "git+git://github.com/rogerbinns/apsw.git@26b61c39a98db3ccad7f852adc944b0b8e94c242#egg=apsw" \
         --global-option="fetch" --global-option="--sqlite" --global-option="--missing-checksum-ok" \
         --global-option="build" --global-option="--enable-all-extensions"
 fi
